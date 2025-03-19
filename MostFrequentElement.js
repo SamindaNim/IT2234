054493a1b63ed0fae43f3a42abd6ea9bb0c980db
Saminda Ranathunga <saminda.nim@gmail.com>
2025-03-19 20:16:37 +0530

    MostFrequentElement.js

--- MostFrequentElement.js
+++ MostFrequentElement.js
@@ -1,2 +1,18 @@
 //Find the most frequent element in the array
-let arr = [4,8,3,4,3,2,1,8,4]
+let arr = [4, 8, 3, 4, 3, 2, 1, 8, 4];
+
+let count = {};
+let maxFreq = 0;
+let mostFreq;
+
+arr.forEach(num => {
+    count[num] = (count[num] || 0) + 1; // Increment count
+
+    if (count[num] > maxFreq) {
+        maxFreq = count[num]; // Update max
+        mostFreq = num; // Update most frequent element
+    }
+});
+
+console.log(arr+"\n")
+console.log("Most frequent element:", mostFreq); // Most frequent element
